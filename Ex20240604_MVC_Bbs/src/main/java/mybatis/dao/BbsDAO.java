6ad99d59eeb8d806fb47a5f4b79ab4fb3e9997ea
wjsxorjs <wjsxorjs@naver.com>
2024-06-04 15:33:42 +0900

    JSP 20240604 0.7

---
+++ Ex20240604_MVC_Bbs/src/main/java/mybatis/dao/BbsDAO.java
@@ -0,0 +1,38 @@
+package mybatis.dao;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import mybatis.service.FactoryService;
+import mybatis.vo.BbsVO;
+
+public class BbsDAO {
+
+	public static BbsVO[] getList(String bname, int begin, int end) {
+		BbsVO[] b_ar = null;
+		
+		SqlSession ss = FactoryService.getFactory().openSession();
+		
+		HashMap<String, String> b_map = new HashMap<>();
+		
+		b_map.put("bname", bname);
+		b_map.put("begin", String.valueOf(begin));
+		b_map.put("end", String.valueOf(end));
+		
+		List<BbsVO> b_list = ss.selectList("bbs.list",b_map);
+		
+		if(b_list != null && b_list.size()>0) {
+			b_ar = new BbsVO[b_list.size()];
+			
+			b_list.toArray(b_ar);
+		}
+		
+		return b_ar;
+	}
+	
+	
+	
+	
+}
